5062978cd5f30236e322d01e4383b2b3eaec06d5
Kaue Lima <kauelyma@gmail.com>
2025-03-19 23:03:53 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿ánamespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
