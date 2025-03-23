adb3829afffdeef774fc693872c30b6ff09e13be
Kaue Lima <kauelyma@gmail.com>
2025-03-23 12:02:51 -0300

    adicionado get set

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,14 +1,15 @@
 ﻿Musica musica1 = new Musica();
-musica1.nome = "Roxane";
-musica1.artista = "The Police";
-musica1.duracao = 300;
-musica1.disponivel = true;
+musica1.Nome = "Roxane";
+musica1.Artista = "The Police";
+musica1.Duracao = 300;
+musica1.Disponivel = (true);
+Console.WriteLine(musica1.Disponivel);
 
 Musica musica2 = new Musica();
-musica2.nome = "Every Breath You Take";
-musica2.artista = "U2";
-musica2.duracao = 300;
-musica2.disponivel = false;
+musica2.Nome = "Every Breath You Take";
+musica2.Artista = "U2";
+musica2.Duracao = 300;
+musica2.Disponivel = (false);
 
 musica1.ExibirFichaTecnica();
 musica2.ExibirFichaTecnica();
